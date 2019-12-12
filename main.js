2335bf0788e9feb619cc19957416d3f3eeaa6995
Anton Kryukov <kryukov.anton.81@gmail.com>
2019-12-12 22:05:34 +0700

    Functional slider

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//полноэкранное меню
 const menuButton = document.getElementById('hamburger-menu');
 
 const fullScreenMenu = document.getElementById('modal');
@@ -19,3 +20,75 @@ menuClose.addEventListener('click', function () {
     fullScreenMenu.classList.remove('full-screen__menu_active');
 
 });
+
+//аккордеон меню (горизонтальный)
+
+const accord1 = document.getElementsByClassName("menu-acco__item");
+
+console.log(accord1);
+
+var width = document.documentElement.clientWidth;
+
+console.log(width);
+
+window.addEventListener('resize', function (e) {
+    width = document.documentElement.clientWidth;
+})
+
+for (var index = 0; index < accord1.length; index++) {
+
+    const element = accord1[index];
+    
+    element.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        for (var i = 0; i < accord1.length; i++) {
+            if (i !== index) {
+                accord1[i].classList.remove('menu-acco__item--active');
+            }
+            
+        }
+        
+        if (element.classList.contains('menu-acco__item--active')) {            
+            element.classList.remove('menu-acco__item--active');
+
+        } else {
+            console.log('add');
+            element.classList.add('menu-acco__item--active');
+        }
+        
+
+        if (width < 650) {
+            let currElem = index + 1;
+            let count = lines.length - currElem;
+            let overWidth = 80 * count;
+            let content = element.getElementsByClassName('menu-acco__content');
+            content[0].style ='right: -' + overWidth+'px';
+        }       
+        
+    })    
+}
+//слайдер
+const left = document.querySelector("#left");
+console.log(left);
+const right = document.querySelector("#right");
+console.log(right);
+const items = document.querySelector("#items");
+console.log(items);
+right.addEventListener('click', function(e) {
+    console.log("сработало");
+    loop("right", e);
+});
+ 
+left.addEventListener('click', function(e) {
+    loop("left", e);
+});
+
+function loop(direction, e) {
+    e.preventDefault();
+    if (direction === "right") {
+        items.appendChild(items.firstElementChild);
+    } else {
+        items.insertBefore(items.lastElementChild, items.firstElementChild);
+  }
+}
