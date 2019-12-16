0243976c283be4304051766ca1741b2ca847cf88
Anton Kryukov <kryukov.anton.81@gmail.com>
2019-12-16 14:09:22 +0700

    OrderForm functional & fullReview__modal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//полноэкранное меню
+//fullScreenMenu
 const menuButton = document.getElementById('hamburger-menu');
 
 const fullScreenMenu = document.getElementById('modal');
@@ -18,6 +18,34 @@ menuClose.addEventListener('click', function () {
     
     body.classList.remove('body_closed');
     fullScreenMenu.classList.remove('full-screen__menu_active');
+
+});
+
+//fullReview__modal
+const moreButton = document.querySelectorAll('button.button__more');
+
+console.log(moreButton);
+
+const fullReview = document.getElementById('fullReview__modal');
+
+const fullReviewClose = document.getElementById('fullReviewClose');
+
+for (var index = 0; index<moreButton.length; index++) {
+
+    moreButton[index].addEventListener('click', function (e) {
+             
+        body.classList.add('body_closed');   
+        fullReview.classList.add('full-review__modal_active');
+    
+    });
+ 
+}
+
+fullReviewClose.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    body.classList.remove('body_closed');
+    fullReview.classList.remove('full-review__modal_active');
 
 });
 
@@ -53,7 +81,7 @@ for (var index = 0; index < accord1.length; index++) {
             element.classList.remove('menu-acco__item--active');
 
         } else {
-            console.log('add');
+            //console.log('add');
             element.classList.add('menu-acco__item--active');
         }
         
@@ -68,7 +96,9 @@ for (var index = 0; index < accord1.length; index++) {
         
     })    
 }
-//слайдер
+
+
+//slider
 const left = document.querySelector("#left");
 console.log(left);
 const right = document.querySelector("#right");
@@ -92,3 +122,75 @@ function loop(direction, e) {
         items.insertBefore(items.lastElementChild, items.firstElementChild);
   }
 }
+
+
+//orderForm functional
+
+const myForm = document.querySelector('#myForm');
+const orderButton = document.querySelector('#orderButton')
+
+orderButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    //console.log(myForm.elements.name.value);
+    //console.log(myForm.elements.phone.value);
+    //console.log(myForm.elements.comment.value);  
+
+    
+    if (validateForm(myForm)) {
+
+       // data.append("to","my@mail.com");
+
+        const data = {
+            name: myForm.elements.name.value,            
+            phone: myForm.elements.phone.value,
+            comment: myForm.elements.comment.value,
+            to: 'my@mail.com'
+        };
+
+        console.log(data);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.send(JSON.stringify(data));
+        
+       
+        
+    }
+
+
+    //if (myForm.elements.callback.checked == true) {
+        //console.log('не перезванивать');
+    //})
+
+    function validateForm(form) {
+        let valid = true;
+
+        if (!validateField(form.elements.name)) {
+            valid = false;
+        }
+
+        if (!validateField(form.elements.phone)) {
+            valid = false;
+        }
+
+        if (!validateField(form.elements.comment)) {
+            valid = false;
+        }
+
+        return valid;
+    }
+
+    function validateField(field) {
+        
+        if (!field.checkValidity()) {         
+           
+            field.nextElementSibling.textContent = field.validationMessage;
+            
+            return false;
+        } else {
+            field.nextElementSibling.textContent = '';
+
+            return true;
+        }
+    }
+})
